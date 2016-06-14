a3ce4f7a6ea94150bae37128444495fe73fd1509
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2016-06-13 23:38:44 -0400

    fixing aabb calc for ellipsoid

--- src/shapes/Ellipsoid.js
+++ src/shapes/Ellipsoid.js
@@ -5,6 +5,9 @@ var Vec3 = require('../math/Vec3');
 
 /**
  * Ellipsoid shape (reference: https://en.wikipedia.org/wiki/Ellipsoid)
+ *
+ *   (x / a)^2 + (y / b)^2 + (z / c)^2 = 1
+ *
  * @class Ellipsoid
  * @constructor
  * @extends Shape
@@ -31,8 +34,8 @@ function Ellipsoid(a, b, c) {
 
     this.type = Shape.types.ELLIPSOID;
 
-    if(this.a < 0 || this.b < 0 || this.c < 0){
-        throw new Error('The Ellipsoid radius cannot be negative.');
+    if(this.a <= 0 || this.b <= 0 || this.c <= 0){
+        throw new Error('The Ellipsoid lengths must be positive.');
     }
 
     this.updateBoundingSphereRadius();
@@ -42,12 +45,12 @@ Ellipsoid.prototype.constructor = Ellipsoid;
 
 Ellipsoid.prototype.calculateLocalInertia = function(mass,target){
     target = target || new Vec3();
-    var I_xx = 2.0*mass * this.b * this.c / 5.0;
-    var I_yy = 2.0*mass * this.a * this.c / 5.0;
-    var I_zz = 2.0*mass * this.b * this.a / 5.0;
-    target.x = I_xx;
-    target.y = I_yy;
-    target.z = I_zz;
+    var a = this.a,
+        b = this.b,
+        c = this.c;
+    target.x = mass * (b*b + c*c) / 5.0;
+    target.y = mass * (a*a + c*c) / 5.0;
+    target.z = mass * (a*a + b*b) / 5.0;
     return target;
 };
 
@@ -60,16 +63,24 @@ Ellipsoid.prototype.updateBoundingSphereRadius = function(){
 };
 
 var rotatedLengthsTemp = new Vec3();
-Ellipsoid.prototype.calculateWorldAABB = function(pos,quat,min,max){
-    rotatedLengthsTemp.set(this.a, this.b, this.c);
-    quat.vmult(rotatedLengthsTemp, rotatedLengthsTemp);
-    var a = rotatedLengthsTemp.x,
-        b = rotatedLengthsTemp.y,
-        c = rotatedLengthsTemp.z;
-    min.x = pos.x - a;
-    max.x = pos.x + a;
-    min.y = pos.y - b;
-    max.y = pos.y + b;
-    min.z = pos.z - c;
-    max.z = pos.z + c;
+Ellipsoid.prototype.calculateWorldAABB = function(pos,quat,min,max) {
+    /**
+     * Points \vec{x} := [x_1, x_2, x_3]^T on the rotated ellipsoid (ignoring translation for now) satisfy the equation
+     *
+     *   f(\vec{x}) := \vec{x}^T A \vec{x} = f(\vec{x})
+     *               = \vec{x}^T U \Sigma U^T \vec{x} = 1,
+     *
+     * where U is the 3x3 orthogonal rotation matrix (computed from the quaternion),
+     * \Sigma is the diagonal matrix of eigenvalues (1/a^2, 1/b^2, 1/c^2).
+     *
+     * The two points attaining min/max in the first component occur where
+     *
+     *   \grad{f}(\vec{x})_2 = 0, \grad{f}(\vec{x})_3 = 0.
+     *
+     * Expression for the gradient:
+     *
+     *   \grad{f}(\vec{x}) = 2 U \Sigma U^T \vec{x}.
+     *
+     */
+
 };
